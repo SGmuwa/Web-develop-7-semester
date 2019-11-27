f2f74fe013d041853f9654c56f280cb44a7e8825
pupysheva <anime-manga@bk.ru>
2019-11-27 10:29:09 +0300

    test import as from

--- project-x/src/index.js
+++ project-x/src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import * as getCounterModule from './getCounter'
 
 function getComponent() {
 	const element = document.createElement('div');
@@ -7,39 +8,12 @@ function getComponent() {
 	// inner - это встраивание между <div> и </div>
 	// lodash - библиотека для работы с массивами.
 	// npx webpack для старта сборки.
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.innerHTML = _.join([getCounterModule.hello, 'webpack'], ' ');
   
 	return element;
-  }
-  document.body.appendChild(getComponent());
+}
+document.body.appendChild(getComponent());
 
-  function getCounter() {
-	const elementDiv = document.createElement('div');
 
-	const elementButtonCounter = document.createElement('button');
-	elementButtonCounter.innerText = '0';
-	
-	const elementButtonConsole = document.createElement('button');
-	elementButtonConsole.innerText = 'on';
-
-	elementDiv.appendChild(elementButtonCounter);
-	elementDiv.appendChild(elementButtonConsole);
-
-	elementButtonCounter.addEventListener('click', event => event.target.innerText++);
-	const loger = event => console.log(event.target.innerText);
-	elementButtonCounter.addEventListener('click', loger);
-
-	elementButtonConsole.addEventListener('click', () => {
-		if(elementButtonConsole.innerText == 'on') {
-			elementButtonCounter.removeEventListener('click', loger);
-			elementButtonConsole.innerText = 'off';
-		} else {
-			elementButtonCounter.addEventListener('click', loger);
-			elementButtonConsole.innerText = 'on';
-		}
-	});
-
-	return elementDiv;
-  }
-  document.body.appendChild(getCounter());
+document.body.appendChild(getCounterModule.getCounter());
   
