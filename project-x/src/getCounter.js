0d1ef0989ac0af9e3890e04a6f5a842c2f82b56e
dkamyshov <danil.kamyshov@gmail.com>
2019-12-11 10:08:04 +0300

    Add setInterval

--- project-x/src/getCounter.js
+++ project-x/src/getCounter.js
@@ -1,29 +1,41 @@
-export function getCounter() { // Из этого файла getCounter доступна другим.ы
-	const elementDiv = document.createElement('div');
+export function getCounter() {
+  // Из этого файла getCounter доступна другим.ы
+  const elementDiv = document.createElement("div");
 
-	const elementButtonCounter = document.createElement('button');
-	elementButtonCounter.innerText = '0';
-	
-	const elementButtonConsole = document.createElement('button');
-	elementButtonConsole.innerText = 'on';
+  const elementButtonCounter = document.createElement("button");
+  elementButtonCounter.innerText = "0";
 
-	elementDiv.appendChild(elementButtonCounter);
-	elementDiv.appendChild(elementButtonConsole);
+  const elementButtonConsole = document.createElement("button");
+  elementButtonConsole.innerText = "on";
 
-	elementButtonCounter.addEventListener('click', event => event.target.innerText++);
-	const loger = event => console.log(event.target.innerText);
-	elementButtonCounter.addEventListener('click', loger);
+  elementDiv.appendChild(elementButtonCounter);
+  elementDiv.appendChild(elementButtonConsole);
 
-	elementButtonConsole.addEventListener('click', () => {
-		if(elementButtonConsole.innerText == 'on') {
-			elementButtonCounter.removeEventListener('click', loger);
-			elementButtonConsole.innerText = 'off';
-		} else {
-			elementButtonCounter.addEventListener('click', loger);
-			elementButtonConsole.innerText = 'on';
-		}
-	});
+  elementButtonCounter.addEventListener(
+    "click",
+    event => event.target.innerText++
+  );
+  const loger = event => console.log(event.target.innerText);
+  elementButtonCounter.addEventListener("click", loger);
 
-	return elementDiv;
+  function adder() {
+    elementButtonCounter.innerText++;
+  }
+
+  let intervalId;
+
+  elementButtonConsole.addEventListener("click", () => {
+    if (elementButtonConsole.innerText == "on") {
+      elementButtonCounter.removeEventListener("click", loger);
+      elementButtonConsole.innerText = "off";
+      intervalId = setInterval(adder, 500);
+    } else {
+      elementButtonCounter.addEventListener("click", loger);
+      elementButtonConsole.innerText = "on";
+      clearInterval(intervalId);
+    }
+  });
+
+  return elementDiv;
 }
-export const hello = 'hello';
+export const hello = "hello";
